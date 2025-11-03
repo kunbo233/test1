81bd418d429d1eb5faa09cf756525c0af010920d
kunbo <kunbo@wps.cn>
2025-11-03 17:58:12 +0800

    feat: test测试

---
+++ src/pages/index2/index.ts
@@ -0,0 +1,26 @@
+import { test2 } from '@/action/test2'
+import { AES } from 'crypto-es'
+Page({
+  data: {
+    hello: {
+      title: 'Hello weapp-vite11111',
+      description: '这是最基础的 weapp-vite 模板，包含快速开发所需的构建与热更新能力。',
+      docs: 'https://vite.icebreaker.top',
+      links: [
+        {
+          text: '复制文档链接',
+          url: 'https://vite.icebreaker.top',
+        },
+        {
+          text: 'GitHub 仓库',
+          url: 'https://github.com/weapp-vite/weapp-vite',
+          variant: 'ghost',
+        },
+      ],
+    },
+  },
+  onClick() {
+    console.log('on click', AES)
+    test2()
+  },
+})
